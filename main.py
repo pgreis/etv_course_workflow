521fc79804360991810dfb00e1865aac90c754e2
pgreis <paulgreis@gmx.de>
2026-05-17 21:11:04 +0200

    add more waiting time in filter

--- main.py
+++ main.py
@@ -119,8 +119,8 @@ def main():
         filter_pipeline = SeleniumPipelineEngine(
             steps=[
                 GoToUrlStep(name="Go to Course Overview", url=os.getenv("COURSE_OVERVIEW_URL"), driver=driver),
-                ClickSectionIsBlockingStep(driver=driver),
-                ClickElementStep(name="Click Filter Button", xpath=filter_locators.filter, add_wait_time=6.5, driver=driver),
+                ClickSectionIsBlockingStep(driver=driver, add_wait_time=5.0),
+                ClickElementStep(name="Click Filter Button", xpath=filter_locators.filter, add_wait_time=12.0, driver=driver),
                 ClickElementStep(name="Click Location Dropdown", xpath=filter_locators.location_filled, driver=driver),
                 ClickElementStep(name="Click Weekday Option", xpath=filter_locators.weekday_filled,  driver=driver),
                 ClickElementStep(name="Click Apply Filter Button", xpath=filter_locators.apply_filter_filled, driver=driver),
